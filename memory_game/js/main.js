3e69a11226cbe4717d55d1556a30b21bee13a8fe
Kristen Moran <moviequeen0219@kristensmacbook.fios-router.home>
2020-03-09 16:49:43 -0400

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,8 @@
+console.log("Up and running!");
+let cardOne = "queen";
+let cardTwo = "king";
+let cardThree = "king";
+let cardFour = "queen";
+
+console.log("User flipped" + cardOne);
+console.log("User flipped" + cardFour);
