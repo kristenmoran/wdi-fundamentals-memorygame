b671e5ffa2eec133bbfe219d0de5a9b1ea9ebbb6
Kristen Moran <moviequeen0219@kristensmacbook.fios-router.home>
2020-03-12 13:42:00 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,31 @@
 console.log("Up and running!")
 
-let cards = ["queen", "queen", "king", "king"];
+let cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+];
+
 let cardsInPlay = [];
 
 function checkForMatch() {
@@ -15,8 +40,10 @@ if (cardsInPlay.length === 2) {
 
 function flipCard(cardId) {
 
-console.log("User flipped " + cards[cardId])
-cardsInPlay.push(cards[cardId]);
+console.log("User flipped " + cards[cardId].rank);
+console.log(cards[cardId].suit);
+console.log(cards[cardId].cardImage);
+cardsInPlay.push(cards[cardId].rank);
 
 checkForMatch();
 
